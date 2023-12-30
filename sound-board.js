6eb93826097396bfd73f4d1f1bbb23e5d1bed2b8
Nkwada Nora Solonko <nkwadanora@mail.com>
2023-12-29 16:14:01 -0800

    add js

--- sound-board.js
+++ sound-board.js
@@ -1,3 +1,25 @@
-const search = document.querySelector('.search')
-const btn = document.querySelector('.btn')
-const input = document.querySelector('.input')
+const sounds = ['birds', 'bonfire', 'fireplace', 'rain']
+
+sounds.forEach(sound => {
+  const btn = document.createElement('button')
+  btn.classList.add('btn')
+
+  btn.innerText = sound
+
+  btn.addEventListener('click', () => {
+    stopSongs()
+
+    document.getElementById(sound).play()
+  })
+
+  document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+  sounds.forEach(sound => {
+    const song = document.getElementById(sound)
+
+    song.pause()
+    song.currentTime = 0;
+  })
+}
